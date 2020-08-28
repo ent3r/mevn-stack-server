3ae78260b187d73f1d275a63b47a4f4a5c88b8b7
ent3r <32072697+ent3r@users.noreply.github.com>
2020-08-28 11:07:06 +0200

    :tada: Add base express app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import * as express from "express";
+import * as bodyparser from "body-parser";
 
 const app = express();
 
+app.use(bodyparser.json());
+app.use(bodyparser.raw());
+
 app.all("/", (req: express.Request, res: express.Response): void => {
-  res.send(`Hello world! Here is the data you sent me: ${req.body?.data}`);
+  res.send(
+    `Hello world! Here is the data you sent me: ${JSON.stringify(req.body)}`
+  );
 });
 
 app.listen(8080, (err) => {
