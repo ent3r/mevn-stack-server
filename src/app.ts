e868ee7625c15da45ebc3b746b09ebd60702dcaf
ent3r <32072697+ent3r@users.noreply.github.com>
2020-10-03 23:51:13 +0200

    :sparkles: Config management using convict
    1. :heavy_plus_sign: Add convict to deps
    2. :construction: Create config management using convict

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express = require("express");
 import { json } from "body-parser";
+import config from "./config/index";
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.all("/", (req: express.Request, res: express.Response): void => {
   );
 });
 
-app.listen(8080, () => {
-  console.log("Listening on 8080");
+const port = config.get("port");
+const ip = config.get("ip");
+
+app.listen(port, ip, () => {
+  console.log(`Listening on ${ip}:${port}`);
 });
---
+++ src/config/index.ts
@@ -0,0 +1,48 @@
+import convict = require("convict");
+import { ipaddress } from "convict-format-with-validator";
+import { resolve } from "path";
+
+convict.addFormat(ipaddress);
+
+const config = convict({
+  env: {
+    doc: "The application environment.",
+    format: ["production", "development", "test"],
+    default: "development",
+    env: "NODE_ENV",
+  },
+  ip: {
+    doc: "The IP address to bind.",
+    format: "ipaddress",
+    default: "127.0.0.1",
+    env: "HOST",
+    arg: "host",
+  },
+  port: {
+    doc: "The port to bind.",
+    format: "port",
+    default: 8080,
+    env: "PORT",
+    arg: "port",
+  },
+  db: {
+    host: {
+      doc: "Database host name/IP and port",
+      format: "*",
+      default: "mongodb://localhost:27017",
+      env: "MONGO_URI",
+    },
+    db: {
+      doc: "Database name",
+      format: String,
+      default: "mevn-stack",
+      env: "MONGO_DB_NAME",
+    },
+  },
+});
+
+config.loadFile(resolve(__dirname, `.env.${config.get("env")}.json`));
+
+config.validate({ allowed: "strict" });
+
+export default config;
