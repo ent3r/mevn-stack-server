1858777e476c3813c989fb157878ee766da3692c
ent3r <32072697+ent3r@users.noreply.github.com>
2020-10-05 17:15:54 +0200

    :recycle: Reorder some properties

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -3,25 +3,25 @@ import { Schema, Document, model } from "mongoose";
 export interface IPostSchema {
   title: string;
   body: string;
+  author: string;
   publishedAt: Date;
   lastEditedAt: Date;
-  author: string;
 }
 
 export const PostSchema = new Schema<IPostSchema>({
   title: String,
   body: String,
+  author: String,
   publishedAt: Date,
   lastEditedAt: Date,
-  author: String,
 });
 
 export interface IPostModel extends Document {
   title: string;
   body: string;
+  author: string;
   publishedAt: Date;
   lastEditedAt: Date;
-  author: string;
 }
 
 export const PostModel = model<IPostModel>("post", PostSchema);
