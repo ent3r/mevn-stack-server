55b85d576b3dd1505e5ff55114ad60a74248bf81
ent3r <32072697+ent3r@users.noreply.github.com>
2020-10-09 14:48:31 +0200

    :recycle: Update how routes are handled + add 405 handler
    Routes are now defined using .route() with all the methods following that. That way its easier to define .all() near the end for the 405 handler

--- src/api/routes/post.ts
+++ src/api/routes/post.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { celebrate, Joi } from "celebrate";
 
 import PostService from "../../services/posts";
@@ -10,72 +10,76 @@ const postIDValidation = celebrate({
 });
 
 export default (router: Router): void => {
-  router.post(
-    "/posts",
-    celebrate({
-      body: Joi.object({
-        title: Joi.string().min(5).max(120).required(),
-        body: Joi.string().min(1).max(480).required(),
-        author: Joi.string().min(2).max(50).optional().default("Anon"),
-      }),
-    }),
-    async (req: Request, res: Response): Promise<Response<any>> => {
-      return postService
-        .createNewPost({
-          title: req.body.title,
-          body: req.body.body,
-          author: req.body.author,
-        })
-        .then((documentID: string) => res.status(201).send(documentID));
-    }
-  );
-
-  router.patch(
-    "/posts/:postID",
-    celebrate(
-      {
-        params: Joi.object({ postID: Joi.string().length(24).required() }),
+  router
+    .route("/posts")
+    .get(
+      async (req: Request, res: Response): Promise<Response<any>> => {
+        return postService
+          .getPosts()
+          .then((posts) => res.status(200).send(posts));
+      }
+    )
+    .post(
+      celebrate({
         body: Joi.object({
-          title: Joi.string().min(5).max(120),
-          body: Joi.string().min(1).max(480),
-          author: Joi.string().min(2).max(50),
-        }).min(1),
-      },
-      { messages: { "object.min": "body must have at least {#limit} key" } }
-    ),
-    async (req: Request, res: Response): Promise<Response<any>> => {
-      return postService
-        .updatePost(req.params.postID, req.body)
-        .then((newDoc) => res.status(200).send(newDoc._id));
-    }
-  );
+          title: Joi.string().min(5).max(120).required(),
+          body: Joi.string().min(1).max(480).required(),
+          author: Joi.string().min(2).max(50).optional().default("Anon"),
+        }),
+      }),
+      async (req: Request, res: Response): Promise<Response<any>> => {
+        return postService
+          .createNewPost({
+            title: req.body.title,
+            body: req.body.body,
+            author: req.body.author,
+          })
+          .then((documentID: string) => res.status(201).send(documentID));
+      }
+    )
+    .all((req: Request, res: Response, next: NextFunction): void => {
+      const err = new Error("Method Not Allowed");
+      err["status"] = 405;
+      next(err);
+    });
 
-  router.delete(
-    "/posts/:postID",
-    postIDValidation,
-    async (req: Request, res: Response): Promise<Response<any>> => {
-      return postService
-        .deletePost(req.params.postID)
-        .then(() => res.status(204).send());
-    }
-  );
-
-  router.get(
-    "/posts",
-    async (req: Request, res: Response): Promise<Response<any>> => {
-      return postService
-        .getPosts()
-        .then((posts) => res.status(200).send(posts));
-    }
-  );
-
-  router.get(
-    "/posts/:postID",
-    postIDValidation,
-    async (req: Request, res: Response): Promise<Response<any>> => {
-      return postService
-        .getPost(req.params.postID)
-        .then((post) => res.status(200).send(post));
-    }
-  );
+  router
+    .route("/posts/:postID")
+    .all(postIDValidation)
+    .get(
+      async (req: Request, res: Response): Promise<Response<any>> => {
+        return postService
+          .getPost(req.params.postID)
+          .then((post) => res.status(200).send(post));
+      }
+    )
+    .patch(
+      celebrate(
+        {
+          body: Joi.object({
+            title: Joi.string().min(5).max(120),
+            body: Joi.string().min(1).max(480),
+            author: Joi.string().min(2).max(50),
+          }).min(1),
+        },
+        { messages: { "object.min": "body must have at least {#limit} key" } }
+      ),
+      async (req: Request, res: Response): Promise<Response<any>> => {
+        return postService
+          .updatePost(req.params.postID, req.body)
+          .then((newDoc) => res.status(200).send(newDoc._id));
+      }
+    )
+    .delete(
+      async (req: Request, res: Response): Promise<Response<any>> => {
+        return postService
+          .deletePost(req.params.postID)
+          .then(() => res.status(204).send());
+      }
+    )
+    .all((req: Request, res: Response, next: NextFunction): void => {
+      const err = new Error("Method Not Allowed");
+      err["status"] = 405;
+      next(err);
+    });
 };
